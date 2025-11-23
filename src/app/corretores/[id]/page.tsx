8ec0c47bd844125c11570a80ae9fbb960c73294b
Felipe-MML <felipemaras20@gmail.com>
2025-11-23 11:58:34 -0300

    refactor: Alteração do estilo da tela de visualização única de corretores

--- src/app/corretores/[id]/page.tsx
+++ src/app/corretores/[id]/page.tsx
@@ -5,6 +5,15 @@ import { useParams } from "next/navigation";
 import PrivateRoute from "@/components/privateRoute";
 import { getCorretorById, Corretor } from "@/services/corretorService";
 
+import {
+  FaUser,
+  FaEnvelope,
+  FaPhone,
+  FaIdCard,
+  FaFingerprint,
+  FaCalendar,
+} from "react-icons/fa6";
+
 const CorretorProfileContent = () => {
   const params = useParams();
   const id = params.id as string;
@@ -41,7 +50,7 @@ const CorretorProfileContent = () => {
   }
 
   return (
-    <div className="w-full max-w-4xl mx-auto">
+    <div className="w-full max-w-350 mx-auto">
       <div className="bg-white p-8 rounded-lg shadow-md">
         <div className="flex flex-col items-center md:flex-row md:items-start space-x-0 md:space-x-8">
           <div className="w-full mt-4 md:mt-0">
@@ -51,44 +60,68 @@ const CorretorProfileContent = () => {
             <p className="text-md text-gray-500 capitalize">
               {corretor.perfil}
             </p>
+
             <div className="mt-6">
               <div className="flex flex-col md:flex-row md:space-x-12">
                 <div className="flex-1 space-y-4">
-                  <h2 className="text-xl font-semibold text-gray-700 border-b pb-2">
-                    Contato
+                  <h2 className="flex gap-2 items-center text-xl font-semibold text-gray-700 border-b pb-2">
+                    <FaUser className="text-blue-700" /> Contato
                   </h2>
-                  <div className="flex flex-col">
-                    <span className="text-sm font-semibold text-gray-500">
-                      Email
-                    </span>
-                    <p className="text-gray-800">{corretor.email}</p>
+                  <div className="flex bg-gray-200 p-2 rounded-2xl flex-col">
+                    <div className="flex items-center gap-2">
+                      <div className="bg-blue-200 p-2 rounded-2xl">
+                        <FaEnvelope className="text-2xl text-blue-600" />
+                      </div>
+                      <div>
+                        <span className="text-sm font-semibold text-gray-500">
+                          Email
+                        </span>
+                        <p className="text-gray-800">{corretor.email}</p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex flex-col">
-                    <span className="text-sm font-semibold text-gray-500">
-                      Telefone
-                    </span>
-                    <p className="text-gray-800">{corretor.telefone}</p>
+
+                  <div className="flex items-center gap-2 bg-gray-200 p-2 rounded-2xl">
+                    <div className="bg-green-200 p-2 rounded-2xl">
+                      <FaPhone className="text-2xl text-green-600" />
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-sm font-semibold text-gray-500">
+                        Telefone
+                      </span>
+                      <p className="text-gray-800">{corretor.telefone}</p>
+                    </div>
                   </div>
                 </div>
                 <div className="flex-1 space-y-4 mt-6 md:mt-0">
-                  <h2 className="text-xl font-semibold text-gray-700 border-b pb-2">
-                    Documentação
+                  <h2 className="flex items-center gap-2 text-xl font-semibold text-gray-700 border-b pb-2">
+                    <FaIdCard className="text-purple-700 " /> Documentação
                   </h2>
-                  <div className="flex flex-col">
-                    <span className="text-sm font-semibold text-gray-500">
-                      CPF
-                    </span>
-                    <p className="text-gray-800">{corretor.cpf}</p>
+                  <div className="flex items-center gap-2 bg-gray-200 p-2 rounded-2xl">
+                    <div className="bg-purple-200 p-2 rounded-2xl">
+                      <FaFingerprint className="text-2xl text-purple-600" />
+                    </div>
+                    <div>
+                      <span className="text-sm font-semibold text-gray-500">
+                        CPF
+                      </span>
+                      <p className="text-gray-800">{corretor.cpf}</p>
+                    </div>
                   </div>
-                  <div className="flex flex-col">
-                    <span className="text-sm font-semibold text-gray-500">
-                      Membro Desde
-                    </span>
-                    <p className="text-gray-800">
-                      {new Date(corretor.data_cadastro).toLocaleDateString(
-                        "pt-BR"
-                      )}
-                    </p>
+                  <div className="flex items-center gap-2 bg-gray-200 p-2 rounded-2xl">
+                    <div className="bg-yellow-200 p-2 rounded-2xl">
+                      <FaCalendar className="text-2xl text-yellow-600 mx-auto" />
+                    </div>
+                    <div>
+                      <span className="text-sm font-semibold text-gray-500">
+                        Membro Desde
+                      </span>
+                      <p className="text-gray-800">
+                        {new Date(corretor.data_cadastro).toLocaleDateString(
+                          "pt-BR"
+                        )}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
